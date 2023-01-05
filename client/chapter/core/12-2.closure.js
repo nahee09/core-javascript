d4bbe01d934ca27877a9041a3df086c066337758
nahee09 <tink159976@gmail.com>
2023-01-05 11:26:53 +0900

    closure 간단 실무 예제

--- client/chapter/core/12-2.closure.js
+++ client/chapter/core/12-2.closure.js
@@ -22,5 +22,30 @@ function earth(){
   }
 }
 
-const ufo = earth();
+const ufo = earth();//새로운 환경이 만들어진 것 (참조는 아니고 복사도 아니고)
 
+
+//간단 실무 예제
+function handler(){
+
+  let isClicked = false;
+
+  return function () {
+
+    if(isClicked === true){
+      
+      this.style.background = 'transparent';
+    }else{
+      this.style.background = 'red';
+    }
+    isClicked = !isClicked;
+  }
+}
+
+
+document.querySelector('.first').addEventListener('click',handler())
+//여기서는 this가 document.querySelector('.first')
+
+
+
+// isClicked를 접근할 수 없음.

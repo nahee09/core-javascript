b421454b77909124eda0158d94df427d81394066
nahee09 <tink159976@gmail.com>
2023-01-12 09:27:53 +0900

    살짝 정리

--- client/chapter/dom/07.bubble.js
+++ client/chapter/dom/07.bubble.js
@@ -9,11 +9,13 @@ const news = getNode('.news');
 const desc = getNode('.desc');
 
 visual.addEventListener('click', function(e){
-  console.log("target: ", e.target);
-  console.log('currentTarget: ', e.currentTarget);
-  console.log(this === e.currentTarget);
-  console.log('%c visual', 'background:dodgerblue'); //오 이건 콘솔창에도 색 넣는 방법
-  css('.pop', 'display', 'block');
+  console.log("target: ", e.target); //내가 클릭하는 곳
+  console.log('currentTarget: ', e.currentTarget); //이벤트가 걸린 대상 visual클래스 태그
+  console.log(this === e.currentTarget); //true
+  console.log('this: ', this);//visual 클래스 태그
+  //오 아래처럼 쓰면 콘솔창에도 색을 넣을 수 있네.
+  console.log('%c visual', 'background:dodgerblue');//visual 
+  css('.pop', 'display', 'block'); 
 })
 
 getNode('.pop').addEventListener('click', (e)=>{

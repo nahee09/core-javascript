1e99720f3527d1d6fc0c6f0b602879dd0645b56d
nahee09 <tink159976@gmail.com>
2023-01-13 10:06:19 +0900

    내가 먼저 해본 거(마구잡이, 25분 걸림)

--- client/main.js
+++ client/main.js
@@ -1,1 +1,25 @@
-console.log(window);
+// 1. 인풋 밸류값 가져오기 
+// 2. 이벤트 핸들러 연결하기 
+// 3. 이벤트 기본동작 차단하기 
+// 4. 두 수 더해주기 
+// 5. 화면에 출력하기
+/* eslint no-undef:'warn' */
+/* eslint no-unused-vars: 'off' */
+
+const firstNumber = getNode('#firstNumber');
+const secondNumber = getNode('#secondNumber');
+const calculator = getNode('.calculator');
+const result = getNode('.result');
+
+
+function handler(e){
+  let target = e.target;
+  if(target.value === 'done'){
+    e.preventDefault();
+    const sumResult = +firstNumber.value + +secondNumber.value;
+    result.innerText = sumResult;
+  }
+
+}
+
+calculator.addEventListener('click', handler);

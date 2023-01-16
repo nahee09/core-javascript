05736c632d34aa60946318ff907fe93ba5b74c4b
nahee09 <tink159976@gmail.com>
2023-01-16 15:55:30 +0900

    memoization함수 만들기(캐시 만들기, 클로저 활용)

---
+++ client/lib/utils/memo.js
@@ -0,0 +1,22 @@
+//memoization (클로저도 이용)
+export const memo = (() => {
+  const cache = {};
+
+  return (key, callback) => {
+    if(!callback) return cache[key];
+
+    if(cache[key]){
+      console.warn(`${key}값은 이미 캐시된 값이 존재합니다.`);
+      return;
+    } 
+
+    cache[key] = callback();
+    console.log(cache)
+  }
+})();
+
+// memo('name', 'tiger');
+// memo('cube', ()=> document.querySelector('#cube')); //set
+// memo('cube', ()=> '이것은 큐브'); //warn
+// console.log(memo('cube')); //get
+

2915c741c4d83f0f28c567844f226c3920600517
nahee09 <tink159976@gmail.com>
2023-01-16 15:55:00 +0900

    기록창에 회차, 기록, 합계 뿌려주는 renderRecordListItem함수 만들기, handleReset함수 보완하기

--- client/main.js
+++ client/main.js
@@ -3,7 +3,13 @@ import {
   enableElement,
   disableElement,  
   getNode, 
-  getNodes
+  getNodes, 
+  visibleElement, 
+  invisibleElement, 
+  insertLast, 
+  attr,
+  clearContents, 
+  memo
   } 
   from "./lib/index.js";
 
@@ -22,6 +28,34 @@ import {
 
 //유사배열(nodeList)로 받아와서 배열의 구조 분해 할당
 const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper');
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   render                                   */
+/* -------------------------------------------------------------------------- */
+let index = 0; 
+let sumResult = 0; 
+
+function renderRecordListItem(){
+  const cube = getNode('#cube');
+  let diceValue = attr(cube, 'data-dice');
+  sumResult += +diceValue;
+  let template = /*html*/`
+    <tr></tr>
+      <td>${++index}</td>
+      <td>${diceValue}</td>
+      <td>${sumResult}</td>
+    </tr>`;
+  insertLast('.recordListWrapper tbody', template);
+
+  //값이 쌓일 때마다 밑 지점을 나타내기(스크롤 길이만큼 찍어준다.)
+  recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
+}
+/* -------------------------------------------------------------------------- */
+/*                                    event                                   */
+/* -------------------------------------------------------------------------- */
 
 const handlerRollingDice = (() => {
   let stopAnimation;
@@ -43,4 +77,21 @@ const handlerRollingDice = (() => {
   }
 })()
 
+//기록 버튼 클릭시
+function handleRecord(){
+  visibleElement(recordListWrapper);
+
+  renderRecordListItem();
+}
+
+//초기화 버튼 클릭시
+function handleReset(){
+  index = 0; 
+  sumResult = 0; 
+  invisibleElement(recordListWrapper);
+  clearContents('.recordListWrapper tbody');
+}
+
 rollingDiceButton.addEventListener('click', handlerRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);

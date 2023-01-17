d5e91780ff56d8b474b31675d45f24b6f906f8a8
nahee09 <tink159976@gmail.com>
2023-01-17 14:27:25 +0900

    get통신 함수 자꾸 develop

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -32,53 +32,115 @@
 // }
 
 //POST 통신
-function xhrData(method, url, body){
+// function xhrData(method, url, body){
+//   const xhr = new XMLHttpRequest();
+//     //비동기 통신 오픈
+//   xhr.open(method, url);
+//   xhr.addEventListener('readystatechange', ()=>{
+//     //객체 구조 분해 할당(변수명이 객체의 프로퍼티와 이름이 똑같아야 함)
+//     const {status, readyState, response} = xhr; 
+//     if(status >= 200 && status < 400){
+//       if(readyState === 4){
+//         console.log('통신 성공');
+//         console.log(JSON.parse(response)); //객체화
+//       }
+//     }else{
+//       console.error('통신 실패');
+//     }
+//   });
+
+//   //서버에 요청
+//   xhr.send(JSON.stringify(body)); //문자화
+// }
+
+function xhrData({//기본값 설정
+  method = 'GET', 
+  url = '', 
+  body = null,  
+  onSuccess = null, 
+  onFail = null,
+  headers = {
+    'Content-Type': 'application/json', //이건 적어줘야 함
+    'Access-Control-Allow-Origin' : '*' //이걸 적어놔도 우리가 해결할 수 있는 문제가 아님
+  }
+}){
   const xhr = new XMLHttpRequest();
-    //비동기 통신 오픈
+  //역시 객체 구조 분해 할당,
+  // const {method, url, body} = options;// 근데 위에 파라미터에서 바로 구조분해할당. 
+  //비동기 통신 오픈
   xhr.open(method, url);
-  console.log(xhr)
+
+  // Object.entries(headers).forEach(([key, value]) => { //이것도 구조 분해 할당 활용한거구나. 
+  //   xhr.setRequestHeader(key, value);
+  // });
+
   xhr.addEventListener('readystatechange', ()=>{
-    //객체 구조 분해 할당
-    const {status, readyState, response} = xhr; 
-    if(status >= 200 && status < 400){
-      if(readyState === 4){
-        console.log('통신 성공');
-        console.log(JSON.parse(response)); //객체화
-      }
-    }else{
-      console.error('통신 실패');
+  //객체 구조 분해 할당(변수명이 객체의 프로퍼티와 이름이 똑같아야 함)
+  const {status, readyState, response} = xhr; 
+  if(status >= 200 && status < 400){
+    if(readyState === 4){
+      console.log('통신 성공');
+      onSuccess(JSON.parse(response)); //객체화
+      // return JSON.parse(response); //비동기 통신을 하고 있어서 return이 안됨. 
     }
-  });
+  }else{
+    onFail('통신 실패');
+    // console.error('통신 실패');
+  }
+});
 
   //서버에 요청
   xhr.send(JSON.stringify(body)); //문자화
-  
 }
 
 // xhrData('GET', 'https://jsonplaceholder.typicode.com/users');
-xhrData('POST', 'https://jsonplaceholder.typicode.com/users', {
-    "name": "Kimnahee",
-    "username": "nahee",
-    "email": "nnn@xxxx.com",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "010-1234-5678",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-});
 
+// xhrData('POST', 'https://jsonplaceholder.typicode.com/users', {
+//     "name": "Kimnahee",
+//     "username": "nahee",
+//     "email": "nnn@xxxx.com",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
+//     },
+//     "phone": "010-1234-5678",
+//     "website": "hildegard.org",
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+// });
+
+//얘는 인수를 객체하나로만 받을 때 
+// xhrData({
+//   url: 'https://jsonplaceholder.typicode.com/users',
+//   method:'GET',
+//   body: null,
+//   headers: {
+//       'Content-Type': 'application/json'
+//   }
+// });
+
+// console.log(xhrData({
+//   url: 'https://jsonplaceholder.typicode.com/users'
+// }));//undefined 나옴
+
+xhrData({
+  url: 'https://jsonplaceholder.typicode.com/users', 
+  onSuccess: (result)=>{
+    console.log(result);
+  }, 
+  onFail: (err)=>{
+    console.error(err);
+  }
+});
 
 
 

5b151fdc183c8a8c24cbcc89ae4845b08a18f21f
nahee09 <tink159976@gmail.com>
2023-01-17 16:04:55 +0900

    주석 처리

--- client/main.js
+++ client/main.js
@@ -1,13 +1,13 @@
 import { xhrData, insertLast} from "./lib/index.js";
 // import { } from "./lib/index.js";
 
-xhrData.get(
-  'https://jsonplaceholder.typicode.com/uses',
-  (result) => {
-    console.log(result);
-    insertLast('body', JSON.stringify(result));
-  }, 
-  (err) => {
-    insertLast('body', err);
-  }
-);
+// xhrData.get(
+//   'https://jsonplaceholder.typicode.com/uses',
+//   (result) => {
+//     console.log(result);
+//     insertLast('body', JSON.stringify(result));
+//   }, 
+//   (err) => {
+//     insertLast('body', err);
+//   }
+// );

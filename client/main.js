e287df757a8b49dbda991d7b090c6cccba3c9327
nahee09 <tink159976@gmail.com>
2023-01-17 15:26:09 +0900

    xhrData의 get메서드 실행 해봄

--- client/main.js
+++ client/main.js
@@ -1,1 +1,13 @@
-import {  } from "./lib/index.js";
+import { xhrData, insertLast} from "./lib/index.js";
+// import { } from "./lib/index.js";
+
+xhrData.get(
+  'https://jsonplaceholder.typicode.com/uses',
+  (result) => {
+    console.log(result);
+    insertLast('body', JSON.stringify(result));
+  }, 
+  (err) => {
+    insertLast('body', err);
+  }
+);

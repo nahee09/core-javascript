b2cae8203298b3a852a6fe00dbbeb2b6be9b3597
nahee09 <tink159976@gmail.com>
2023-01-17 12:02:33 +0900

    GET통신과 POST통신

---
+++ client/lib/utils/xhr.js
@@ -0,0 +1,86 @@
+/* readyState
+0: uninitalized //초기화
+1: loading // 로딩 중
+2: loaded //로딩 완료
+3:interative //작동 중
+4:complete //완료
+ */
+
+// GET통신 
+// function xhrData(method, url){
+//   const xhr = new XMLHttpRequest();
+//     //비동기 통신 오픈
+//   xhr.open('GET', url);
+//   //readystate가 변경됐을 때 발생하는 이벤트
+//   xhr.addEventListener('readystatechange', ()=>{
+//     if(xhr.status >= 200 && xhr.status < 400){
+//       if(xhr.readyState === 4){
+//         console.log('통신 성공');
+//         //링크의 JSON데이터가 나옴(아직 string)
+//         console.log(xhr.response); 
+//         //JSON데이터가 JSON.parse()로 객체화 시켜서 객체가 담긴 배열이 됨
+//         console.log(JSON.parse(xhr.response)); 
+//       }
+//     }else{
+//       console.error('통신 실패');
+//     }
+//   });
+
+//   //서버에 요청
+//   xhr.send();
+  
+// }
+
+//POST 통신
+function xhrData(method, url, body){
+  const xhr = new XMLHttpRequest();
+    //비동기 통신 오픈
+  xhr.open(method, url);
+  console.log(xhr)
+  xhr.addEventListener('readystatechange', ()=>{
+    //객체 구조 분해 할당
+    const {status, readyState, response} = xhr; 
+    if(status >= 200 && status < 400){
+      if(readyState === 4){
+        console.log('통신 성공');
+        console.log(JSON.parse(response)); //객체화
+      }
+    }else{
+      console.error('통신 실패');
+    }
+  });
+
+  //서버에 요청
+  xhr.send(JSON.stringify(body)); //문자화
+  
+}
+
+// xhrData('GET', 'https://jsonplaceholder.typicode.com/users');
+xhrData('POST', 'https://jsonplaceholder.typicode.com/users', {
+    "name": "Kimnahee",
+    "username": "nahee",
+    "email": "nnn@xxxx.com",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "010-1234-5678",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+});
+
+
+
+
+
+

09a6afa0db417b19f2bb2254ad2019f0ffd61ce3
nahee09 <tink159976@gmail.com>
2023-01-18 15:52:43 +0900

    get, post, put, delete 메서드 추가, 사용해보기

--- client/lib/utils/nahee.js
+++ client/lib/utils/nahee.js
@@ -5,19 +5,19 @@ const defaultOptions = {
   cache: 'no-cache', 
   credential: 'same-origin', 
   redirect: 'follow', 
-  referrerPolicy: 'no-reffere', 
+  referrerPolicy: 'no-referrer', 
   headers:{
     'Content-Type':'application/json; charset=UTF-8'
   }
 }
 
-const nahee = async (options = {}) => {
+export const nahee = async (options = {}) => {
 
-  const {url, ...restOptions} = {
+  const {url, ...restOptions} = { //url은 왜 써준건지 모르겠네
     ...defaultOptions, 
     ...options, 
-    headers: {...defaultOptions.headers, ...options.headers}
-  }
+    headers: {...defaultOptions.headers, ...(options.headers ?? {})}
+  } //얕은 복사여서 객체 안의 객체인 headers는 위와 같이 따로 설정해주어야 한다. 
 
   let response = await fetch(url, restOptions);
 
@@ -28,4 +28,40 @@ const nahee = async (options = {}) => {
   return response;
 }
 
+nahee.get = (url, options) => {
+  return nahee({
+    url, 
+    ...options
+  })
+}
+
+nahee.post = (url, body, options) => {
+  return nahee({
+    method: 'POST',
+    url, 
+    body: JSON.stringify(body), 
+    ...options
+  })
+}
+
+nahee.put = (url, body, options) => {
+  return nahee({
+    method: 'PUT',
+    url, 
+    body: JSON.stringify(body), 
+    ...options
+  })
+}
+
+nahee.delete = (url, options) => {
+  return nahee({
+    method: 'DELETE',
+    url, 
+    ...options
+  })
+}
+
 // console.log(await nahee());
+
+let response = await nahee.get('https://jsonplaceholder.typicode.com/users');
+console.log(response);

d6e5b767b02092e50a07227c4cf3df5eaa215ecc
nahee09 <tink159976@gmail.com>
2023-01-19 10:52:01 +0900

    index.html .first를 없앴더니 에러떠서 주석처리

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -13,15 +13,16 @@ function delay(callback, timeout = 500){
   setTimeout(callback, timeout);
 }
 
-delay(()=>{
-  first.style.top = '-100px';
-  delay(()=>{
-    first.style.transform = 'rotate(360deg)';
-    delay(()=>{
-      first.style.top = '0px';
-    })
-  })
-})
+// 콜백 지옥
+// delay(()=>{
+//   first.style.top = '-100px';
+//   delay(()=>{
+//     first.style.transform = 'rotate(360deg)';
+//     delay(()=>{
+//       first.style.top = '0px';
+//     })
+//   })
+// })
 
 /* -------------------------------------------------------------------------- */
 /*                                   Promise                                  */
@@ -100,4 +101,4 @@ async function ramyeon(){
   console.log('파 넣기');
 }
 
-ramyeon()
+// ramyeon()

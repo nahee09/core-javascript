2a427f6a17d31347790980bf38e6cfdee20a5a32
nahee09 <tink159976@gmail.com>
2023-01-19 12:00:01 +0900

    gsap 사용, 로딩중 만들기

--- client/main.js
+++ client/main.js
@@ -1,8 +1,13 @@
+/* global gsap */
+
 import { 
   // createUserCard,
   nahee, 
   getNode, 
-  renderUserCard
+  renderUserCard,
+  changeColor,
+  delayP, 
+  renderSpinner
 }from "./lib/index.js";
 // import { } from "./lib/index.js";
 
@@ -50,17 +55,43 @@ import {
 const userCardContainer = getNode('.user-card-inner');
 
 async function rendingUserList(){
-  // let response = await nahee.get('https://jsonplaceholder.typicode.com/users/1');
-  let response = await nahee.get('https://jsonplaceholder.typicode.com/users');
-  let userData = response.data;
+  //로딩 보여주기
+  renderSpinner(userCardContainer);
+  try{
+    //서버에서 받아오느라 딜레이 걸렸다고 설정(가정)
+    await delayP(2000); 
 
-  userData.forEach((data) =>{
-    renderUserCard(userCardContainer, data);
-  })
-  
-  // console.log(createUserCard(userData));
-  // renderUserCard(userCardContainer, userData);
-  
+    //로딩 그만 보여주기
+    getNode('.loadingSpinner').remove();
+
+    // let response = await nahee.get('https://jsonplaceholder.typicode.com/users/1');
+    let response = await nahee.get('https://jsonplaceholder.typicode.com/users');
+    let userData = response.data;
+
+    userData.forEach((data) =>{
+      renderUserCard(userCardContainer, data);
+    })
+    
+    // console.log(createUserCard(userData));
+    // renderUserCard(userCardContainer, userData);
+    
+    // let list = document.querySeletorAll('.user-card');
+    // let realist = Array.from(list);
+    // 위 두 줄을 gsap으로 한 줄로
+    // console.log(gsap.utils.toArray('.user-card'));
+    
+    changeColor('.user-card')
+
+    // 첫 번째 인자에 객체는 못 옴. 
+    gsap.to(gsap.utils.toArray('.user-card'), 
+    {x:0, 
+    duration:1, 
+    stagger:0.2, 
+    opacity:1
+  });
+  }catch(err){
+    console.log(err);
+  }
 }
 
 rendingUserList();

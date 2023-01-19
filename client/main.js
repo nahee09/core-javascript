a1495e11d5b0099561825246942031ceeee97ffd
nahee09 <tink159976@gmail.com>
2023-01-19 10:53:19 +0900

    main.js에서 유저 카드 생성 및 랜더링 해보기

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
 import { 
-  xhrData, 
-  insertLast, 
-  xhrPromise, 
+  // createUserCard,
   nahee, 
-  delayP
+  getNode, 
+  renderUserCard
 }from "./lib/index.js";
 // import { } from "./lib/index.js";
 
@@ -29,15 +28,40 @@ import {
 // })
 
 //nahee.js의 get 메서드 써보기 
-async function render(){
+// async function render(){
 
-  await delayP(2000);
-  let response = await nahee.get('https://jsonplaceholder.typicode.com/users')
+//   await delayP(2000);
+//   let response = await nahee.get('https://jsonplaceholder.typicode.com/users')
 
 
 
-  console.log(response.data);
+//   console.log(response.data);
+// }
+
+// render()
+
+
+
+//rendingUserList
+//ajax get user List 
+//유저 카드 생성
+//생성된 카드로 랜더링
+
+const userCardContainer = getNode('.user-card-inner');
+
+async function rendingUserList(){
+  // let response = await nahee.get('https://jsonplaceholder.typicode.com/users/1');
+  let response = await nahee.get('https://jsonplaceholder.typicode.com/users');
+  let userData = response.data;
+
+  userData.forEach((data) =>{
+    renderUserCard(userCardContainer, data);
+  })
+  
+  // console.log(createUserCard(userData));
+  // renderUserCard(userCardContainer, userData);
+  
 }
 
+rendingUserList();
 
-render()
